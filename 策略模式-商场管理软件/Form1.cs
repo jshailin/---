02a809fc61fa0bdac5f52167b0c04a2468f3da3d
海陵 <jshailin@hotmail.com>
2018-06-24 15:43:06 +0800

    添加项目文件。

---
+++ 策略模式-商场管理软件/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 策略模式_商场管理软件
+{
+    /// <summary> 
+    /// 作者：jshailin 
+    /// 建立时间：2018/6/24 15:42:49 
+    /// 版权：2018-2036 
+    /// CLR版本：4.0.30319.42000 
+    /// 修改说明：
+    /// 策略模式_商场管理软件说明：本代码版权归海陵所有 
+    /// 唯一标识：32931afb-50dd-4ff7-baba-b59a30146f11 
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 策略模式-商场管理软件/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 策略模式_商场管理软件
+{
+    /// <summary> 
+    /// 作者：jshailin 
+    /// 建立时间：2018/6/24 15:42:49 
+    /// 版权：2018-2036 
+    /// CLR版本：4.0.30319.42000 
+    /// 修改说明：
+    /// 策略模式_商场管理软件说明：本代码版权归海陵所有 
+    /// 唯一标识：32931afb-50dd-4ff7-baba-b59a30146f11 
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
